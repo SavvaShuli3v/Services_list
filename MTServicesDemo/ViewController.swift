0b0d7ffa78ae14afc9762077940b393875aa66ad
Savva <savva.shuliatev@iss.digital>
2023-09-15 15:22:39 +0300

    Initial Commit

---
+++ MTServicesDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MTServicesDemo
+//
+//  Created by Savva Shuliatev on 15.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
